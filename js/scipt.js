18e078381b18ce5e6d5454ce14ade84b83aea8f8
alaamohamed58 <alaamohamed58999@gmail.com>
2023-02-13 10:55:24 +0200

    slideer

--- js/scipt.js
+++ js/scipt.js
@@ -193,3 +193,63 @@ document
       document.querySelector(".specilization button").textContent = "عرض الكل";
     }
   });
+
+//////////////////////////////
+function carousel() {
+  let carouselSlider = document.querySelector(".slider");
+  let list = document.querySelector(".slide-track");
+  let item = document.querySelectorAll(".slider .slide");
+  let list2;
+
+  const speed = 0.4;
+
+  const width = list.offsetWidth;
+  let x = 0;
+  let x2 = width;
+
+  function clone() {
+    list2 = list.cloneNode(true);
+    carouselSlider.appendChild(list2);
+    list2.style.left = `${width}px`;
+  }
+
+  function moveFirst() {
+    x -= speed;
+
+    if (width >= Math.abs(x)) {
+      list.style.left = `${x}px`;
+    } else {
+      x = width;
+    }
+  }
+
+  function moveSecond() {
+    x2 -= speed;
+
+    if (list2.offsetWidth >= Math.abs(x2)) {
+      list2.style.left = `${x2}px`;
+    } else {
+      x2 = width;
+    }
+  }
+
+  function hover() {
+    clearInterval(a);
+    clearInterval(b);
+  }
+
+  function unhover() {
+    a = setInterval(moveFirst, 10);
+    b = setInterval(moveSecond, 10);
+  }
+
+  clone();
+
+  let a = setInterval(moveFirst, 10);
+  let b = setInterval(moveSecond, 10);
+
+  carouselSlider.addEventListener("mouseenter", hover);
+  carouselSlider.addEventListener("mouseleave", unhover);
+}
+
+carousel();
